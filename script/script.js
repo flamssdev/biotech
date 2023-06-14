e4ee74ed069f8fa8a40be5bcbd6aada84ab9eeb9
Flávio Emanuel <109769529+flamssdev@users.noreply.github.com>
2023-06-14 11:35:04 -0300

    test

--- script/script.js
+++ script/script.js
@@ -1,34 +1,41 @@
 const hamburguer = document.getElementById("hamburguer");
 
 hamburguer.addEventListener("click", () => {
-    document.getElementById("menu-mobile").classList.toggle("active");
-})
+  document.getElementById("menu-mobile").classList.toggle("active");
+});
 
+const menuLinks = document.querySelectorAll("#menu-mobile a");
 
-const galleryWrapper = document.querySelector('.galeria-wrapper');
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener("click", () => {
+    document.getElementById("menu-mobile").classList.remove("active");
+  });
+});
+
+const galleryWrapper = document.querySelector(".galeria-wrapper");
 
 let isDragging = false;
 let startX;
 let scrollLeft;
 
-galleryWrapper.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = e.pageX - galleryWrapper.offsetLeft;
-    scrollLeft = galleryWrapper.scrollLeft;
+galleryWrapper.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  startX = e.pageX - galleryWrapper.offsetLeft;
+  scrollLeft = galleryWrapper.scrollLeft;
 });
 
-galleryWrapper.addEventListener('mouseleave', () => {
-    isDragging = false;
+galleryWrapper.addEventListener("mouseleave", () => {
+  isDragging = false;
 });
 
-galleryWrapper.addEventListener('mouseup', () => {
-    isDragging = false;
+galleryWrapper.addEventListener("mouseup", () => {
+  isDragging = false;
 });
 
-galleryWrapper.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-    e.preventDefault();
-    const x = e.pageX - galleryWrapper.offsetLeft;
-    const walk = (x - startX) * 1; // Ajuste a sensibilidade da rolagem aqui
-    galleryWrapper.scrollLeft = scrollLeft - walk;
+galleryWrapper.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  const x = e.pageX - galleryWrapper.offsetLeft;
+  const walk = (x - startX) * 1; // Ajuste a sensibilidade da rolagem aqui
+  galleryWrapper.scrollLeft = scrollLeft - walk;
 });
